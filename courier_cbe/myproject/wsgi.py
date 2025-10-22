04756802f14b9d072bd889c51566b0d8b2978871
Gabriel Boyan <126890537+gabo05gabriel@users.noreply.github.com>
2025-10-21 22:33:34 -0400

    Modify WSGI configuration for Django project
    Added import for sys and updated Python path.

--- courier_cbe/myproject/wsgi.py
+++ courier_cbe/myproject/wsgi.py
@@ -1,8 +1,12 @@
+import sys
 import os
+
+# Asegúrate de que el directorio myproject esté en el path de Python
+sys.path.append(os.path.join(os.path.dirname(__file__), 'myproject'))
+
+import django
 from django.core.wsgi import get_wsgi_application
 
-# Establecer la variable de entorno para el archivo de configuración de Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-# Crear la aplicación WSGI
 application = get_wsgi_application()
