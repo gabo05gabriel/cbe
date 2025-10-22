bdb9305aada8c9028d017e727688b0bcc290da38
Gabriel Boyan <126890537+gabo05gabriel@users.noreply.github.com>
2025-10-21 22:29:50 -0400

    wsgi

--- courier_cbe/myproject/wsgi.py
+++ courier_cbe/myproject/wsgi.py
@@ -1,6 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Establecer la variable de entorno para el archivo de configuración de Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
+# Crear la aplicación WSGI
 application = get_wsgi_application()
